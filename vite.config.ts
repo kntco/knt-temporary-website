46555b26ffbc717da4dabf17282217366aaffdfe
Yigit Gulay <yigitgulay11@outlook.com>
2025-06-03 23:39:39 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { tempo } from "tempo-devtools/dist/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/knt-temporary-website/",
   optimizeDeps: {
     entries: ["src/main.tsx", "src/tempobook/**/*"],
   },
